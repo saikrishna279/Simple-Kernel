355a8086396e2f83d9cac5216bb9c8426ba4f2f7
saikrishna279 <androidwall.nrt@gmail.com>
2019-05-12 12:06:39 +0530

    Fix errors in script

--- script.sh
+++ script.sh
@@ -6,12 +6,12 @@
 yellow='\033[0;33m'
 white='\033[0m'
 red='\033[0;31m'
-gre='\e[0;32m'
+green='\e[0;32m'
 blue='\033[0;34m'
 cyan='\033[0;36m'
 
 #init stuff
-KERNEL_DIR="${PWD}"
+KERNEL_DIR=`readlink -f .`
 KERNEL="MalO"
 DEVICE="beryllium"
 ARCH="arm64"
@@ -38,21 +38,21 @@ export USE_CCACHE=1
 export CCACHE_DIR=$CCACHE_DIR
 ccache -M 50G
 
-echo "Script has initialzed build with following DIR's \n"
-echo "kerneldir = $KERNELDIR \n"
-echo "ramfs_source = $RAMFS_SOURCE \n"
-echo "ccache dir = $CCACHE_DIR \n"
-echo "Building as ${KBUILD_BUILD_USER}@${KBUILD_BUILD_HOST} \n"
+echo -e "${green}Script has initialzed build with following DIR's${white}"
+echo -e "kerneldir = ${red}${KERNEL_DIR}${white}"
+echo -e "ccache dir = ${red}${CCACHE_DIR}${white}"
+echo -e "Building as ${yellow}${KBUILD_BUILD_USER}${white}@${blue}${KBUILD_BUILD_HOST}${white}"
 
 
 function clean(){
-echo "Cleaning..."
+echo -e "${red}Cleaning...${white}"
 make clean && make mrproper
 rm ${KERNEL_DIR}/out/arch/arm64/boot/Image
+echo -e "${green}Done!${white}"
 }
 
 function build(){
-echo "Starting build now!"
+echo -e "${green}Starting build now!${white}"
 make O="${OUT}" "${KERNEL}_defconfig"
 make O="${OUT}" -j$(nproc --all) &>buildlog.txt & pid=$!
 }
