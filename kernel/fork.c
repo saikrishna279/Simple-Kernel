90c1f11eba56d7136dbee43584e50aeac6375f1e
saikrishna279 <androidwall.nrt@gmail.com>
2019-05-14 22:33:14 +0530

    CPU_BOOST: Boost for 70mS instead of 32mS

--- kernel/fork.c
+++ kernel/fork.c
@@ -1977,10 +1977,10 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 #ifdef CONFIG_CPU_INPUT_BOOST
-	/* Boost CPU to the max for 32 ms when userspace launches an app */
+	/* Boost CPU to the max for 70 ms when userspace launches an app */
 	if (task_is_zygote(current) && cpu_input_boost_within_input(75)) {
-		cpu_input_boost_kick_max(32);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 32);
+		cpu_input_boost_kick_max(70);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 70);
 #endif
 	}
 
